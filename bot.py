ef90d3d0351f1e7a4fb2cbcb49e187310804ddf4
Ko Moe Htet <moehtet17112024@gmail.com>
2026-09-18 19:57:35 +0630

    Add files via upload

--- bot.py
+++ bot.py
@@ -6,21 +6,11 @@ from telegram.ext import Application, CommandHandler, MessageHandler, ContextTyp
 
 TOKEN = os.getenv("BOT_TOKEN")
 
-print ("bot running")
-
 # Channel ID
 CHANNEL_ID = os.getenv("CHANNEL_ID")
 
-# Save folder
-SAVE_DIR = r"C:\tg_bot_photos"
-
-# folder မရှိရင် create လုပ်
-os.makedirs(SAVE_DIR, exist_ok=True)
-
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text ("🔆 Photo များပို့နိုင်ပါပြီ   📍 file size ကြီးသောပုံများပို့ပါက တစ်ပုံခြင်းပို့ပေးပါ   🚨 '' Saveing..... '' ကြာနေပါက ထက်မံပို့ပေးပါ")
-
+    await update.message.reply_text ("🔆 Photo များသိမ်းနိုင်ပါပြီ   📍 file size ကြီးသောပုံများပို့ပါက တစ်ပုံခြင်းပို့ပေးပါ")
 
 async def save_photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     photo = update.message.photo[-1]
@@ -28,44 +18,28 @@ async def save_photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # Telegram server က file ကိုယူ
     file = await context.bot.get_file(photo.file_id)
 
-    print ("User sending photo")
-
-    # file name တစ်ခုလုပ်
-    filename = f"{update.message.from_user.id}_{update.message.message_id}.jpg"
-
     await update.message.reply_text("Saveing.....")
-
-    file_path = os.path.join(SAVE_DIR, filename)
-
-    # PC ထဲ download
-    await file.download_to_drive(file_path)
-
-    await update.message.reply_text("သိမ်းပြီးပါပြီ")
 
     await context.bot.send_photo(
             chat_id=CHANNEL_ID,
             photo=photo.file_id,
             caption=f"User ID: {update.message.from_user.id}"
         )
-    await update.message.reply_text("🤖 bot active time ( 9:00pm to 9:30pm )")
 
-    print ("Saved in PC/Channel")
+    await update.message.reply_text("သိမ်းပြီးပါပြီ")
 
 #myphoto
 async def myphotos(updute: Update, context:ContextTypes.DEFAULT_TYPE):
     await updute.message.reply_text("Owner သို့ဆက်သွယ်ပါ :-: @Moehtetr")
     
-
 #my id
 async def my_id(update: Update, context:ContextTypes.DEFAULT_TYPE):
     user = update.effective_user
     await update.message.reply_text(f"Your ID : {user.id}")
 
-
 #Not Action,unknowcommand
 async def unknown(updute: Update, context:ContextTypes.DEFAULT_TYPE):
     await updute.message.reply_text("Not Action ?")
-
 
 def run_bot():
     app = Application.builder().token(TOKEN).build()
